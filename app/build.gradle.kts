d5b8b8cbfe230209225abeaba1ee7d4ff54e99fc
Aayush Gupta <aayushgupta219@gmail.com>
2024-05-26 20:07:12 +0530

    Bump AGP, plugins and dependencies to latest stable release
    Signed-off-by: Aayush Gupta <aayushgupta219@gmail.com>
    Change-Id: Ibce2e83568155a74f08d3bb5c1ef447d2fd8058c

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -69,16 +69,16 @@ dependencies {
 
     compileOnly(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar"))))
 
-    implementation("androidx.core:core-ktx:1.12.0")
+    implementation("androidx.core:core-ktx:1.13.1")
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.11.0")
+    implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 
     // ViewModel
-    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.7.0")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.8.0")
 
     // Navigation Components
     val navVersion = "2.7.7"
