9d0c79dc4643c3be601dbf372a9c17a79c25c00a
Aayush Gupta <aayushgupta219@gmail.com>
2024-07-24 13:46:01 +0700

    Bump to latest stable AGP, Gradle and plugins
    Signed-off-by: Aayush Gupta <aayushgupta219@gmail.com>
    Change-Id: I6e7efaeae0b065e42b955074d30130e6b1dd46f5

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -71,16 +71,16 @@ dependencies {
     implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("lineagesettings.jar"))))
 
     implementation("androidx.core:core-ktx:1.13.1")
-    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("androidx.preference:preference-ktx:1.2.1")
     testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    androidTestImplementation("androidx.test.ext:junit:1.2.1")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
 
     // ViewModel
-    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.8.0")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.8.3")
 
     // Navigation Components
     val navVersion = "2.7.7"
